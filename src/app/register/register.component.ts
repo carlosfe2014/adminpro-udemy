2de922835ef3c688ad23b8a059504b13416f17b9
User <carlosfe2019@outlook.com>
2020-04-04 22:46:30 -0500

    Se agrego OnDestroy en el login

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,18 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['../login/login.component.css']
 })
-export class RegisterComponent implements OnInit, OnDestroy {
+export class RegisterComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
   }
 
 }
