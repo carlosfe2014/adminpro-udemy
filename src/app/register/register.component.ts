20c2eca410aded002262d398837a49e7932d0723
User <carlosfe2019@outlook.com>
2020-04-04 23:33:48 -0500

    Segundo commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,26 +1,17 @@
-import {Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['../login/login.component.css']
 })
-export class RegisterComponent implements OnInit, OnDestroy, OnChanges {
+export class RegisterComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log("asdsada");
   }
-
-  ngOnDestroy(): void {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-  }
-
-  prueba(){
-
+  carlos(){
   }
 
 }
